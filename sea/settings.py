a3e230dcd8bc6058ac9970852b643da8a092360e
shaowei <shaowei@shouxin168.com>
2019-05-09 17:56:10 +0800

    init

--- sea/settings.py
+++ sea/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,10 +75,15 @@ WSGI_APPLICATION = 'sea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME':'sea',
+    'USER': 'sea',
+    'PASSWORD': 'sea@orderplus',
+    'HOST': '47.112.113.252',
+    'PORT': '3306',
     }
 }
 
@@ -119,3 +125,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+APPEND_SLASH = False
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    )
+}
+
+JWT_AUTH = {
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
+}
---
+++ sea_app/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+
+urlpatterns = [
+    # 注册 登陆
+    # url(r'^login/$', views.LoginView.as_view()),
+]
