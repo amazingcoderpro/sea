852d9abcb570e7bc6dca78dc328b5199b8dbb3d8
Leemon7 <limengqiAliase@163.com>
2019-05-10 11:40:33 +0800

    新增模型类

--- sea_app/models.py
+++ sea_app/models.py
@@ -23,7 +23,7 @@ class Role(models.Model):
     user_id = models.IntegerField(blank=True, null=True, verbose_name="创建者")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
-    menu = models.CharField(max_length=45, blank=True, null=True, verbose_name="菜单权限")
+    menu_list = models.CharField(max_length=45, blank=True, null=True, verbose_name="菜单权限")  # 格式："[1,2,3]"
 
     class Meta:
         # managed = False
@@ -74,6 +74,110 @@ class Store(models.Model):
         db_table = 'store'
 
 
-# class Product(models.Model):
-#     """产品表"""
-#     sku = models.CharField(max_length=64, verbose_name="店铺名称")
+class Product(models.Model):
+    """产品表"""
+    sku = models.CharField(max_length=64, verbose_name="产品标识符")
+    url = models.CharField(max_length=255, blank=True, null=True, verbose_name="产品URL")
+    sale = models.FloatField(verbose_name="销售额")
+    revenue = models.FloatField(verbose_name="收益")
+    image_url = models.CharField(max_length=255, blank=True, null=True, verbose_name="图片URL")
+    thumbnail = models.TextField(verbose_name="缩略图")
+    scan = models.IntegerField(default=0, verbose_name="浏览量")
+    category = models.CharField(max_length=64, verbose_name="类目")
+    price = models.FloatField(verbose_name="产品价格")
+    tag = models.CharField(max_length=64, verbose_name="所属标签")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'product'
+
+
+class Account(models.Model):
+    """Pin账户表"""
+    name = models.CharField(max_length=64, verbose_name="账户名称")
+    email = models.CharField(max_length=255, verbose_name="登陆邮箱")
+    create_time = models.DateTimeField(verbose_name="账号创建时间")
+    type = models.CharField(max_length=64, verbose_name="账户类型")
+    following = models.IntegerField(default=0, verbose_name="关注量")
+    follower = models.IntegerField(default=0, verbose_name="粉丝")
+    state = models.BooleanField(default=True, verbose_name="账号状态")
+    token = models.CharField(max_length=255, verbose_name="账号使用标识")
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name="添加时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="修改时间")
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'account'
+
+
+class Board(models.Model):
+    """Pin Board表"""
+    name = models.CharField(max_length=64, verbose_name="Board名称")
+    follower = models.IntegerField(default=0, verbose_name="粉丝")
+    create_time = models.DateTimeField(verbose_name="Board创建时间")
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name="添加时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="修改时间")
+    account = models.ForeignKey(Account, on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'board'
+
+
+class Pin(models.Model):
+    """Pin 表"""
+    url = models.CharField(max_length=255, blank=True, null=True, verbose_name="Pin URL")
+    description = models.TextField(verbose_name="Pin 描述")
+    like = models.IntegerField(default=0, verbose_name="喜欢量")
+    comment = models.IntegerField(default=0, verbose_name="评论量")
+    repin = models.IntegerField(default=0, verbose_name="转发量")
+    site_url = models.CharField(max_length=255, blank=True, null=True, verbose_name="产品URL")
+    thumbnail = models.TextField(verbose_name="缩略图")
+    publish_time = models.DateTimeField(auto_now_add=True, verbose_name="发布时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+    board = models.ForeignKey(Board, on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'pin'
+
+
+class Rule(models.Model):
+    """规则表"""
+    start_time = models.DateTimeField(verbose_name="发布开始时间")
+    end_time = models.DateTimeField(verbose_name="发布结束时间")
+    delta_time = models.FloatField(verbose_name="发布间隔时间（秒）")
+    low_scan = models.IntegerField(default=0, verbose_name="低浏览量")
+    high_scan = models.IntegerField(default=0, verbose_name="高浏览量")
+    low_sale = models.IntegerField(default=0, verbose_name="低销售额")
+    high_sale = models.IntegerField(default=0, verbose_name="高销售额")
+    category01 = models.CharField(max_length=64, blank=True, null=True, verbose_name="产品类目01")
+    category02 = models.CharField(max_length=64, blank=True, null=True, verbose_name="产品类目02")
+    category03 = models.CharField(max_length=64, blank=True, null=True, verbose_name="产品类目03")
+    tag = models.CharField(max_length=64, blank=True, null=True, verbose_name="规则标签")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+    board = models.ManyToManyField(Board)
+
+    class Meta:
+        # managed = False
+        db_table = 'rule'
+
+
+class PublishRecord(models.Model):
+    """发布记录表"""
+    board = models.ForeignKey(Board, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, blank=True, null=True)
+    state = models.BooleanField(default=True, verbose_name="是否发布")
+    finished = models.BooleanField(default=True, verbose_name="是否发布成功")
+    remark = models.TextField(blank=True, null=True, verbose_name="备注")
+    execute_time = models.DateTimeField(auto_now_add=True, verbose_name="执行时间")
+
+    class Meta:
+        # managed = False
+        db_table = 'publishrecord'
+
