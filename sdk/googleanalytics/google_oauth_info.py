a587dc1c83448bba0dc5f89309569a601bffa7c1
Silence-python <1447385994@qq.com>
2019-05-27 21:26:24 +0800

    google analytics pull data

--- sdk/googleanalytics/google_oauth_info.py
+++ sdk/googleanalytics/google_oauth_info.py
@@ -3,81 +3,82 @@
 from apiclient import discovery
 from oauth2client.service_account import ServiceAccountCredentials
 
-SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
-KEY_FILE_LOCATION = 'client_secrets.json'
-VIEW_ID = '195406097'
 
+class GoogleApi():
+    def __init__(self):
+        self.SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+        self.KEY_FILE_LOCATION = 'client_secrets.json'
+        self.VIEW_ID = '195406097'
 
-def initialize_analyticsreporting():
-    """Initializes an Analytics Reporting API V4 service object.
-    Returns:
-      An authorized Analytics Reporting API V4 service object.
-    """
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(KEY_FILE_LOCATION, SCOPES)
-    # Build the service object.
-    analytics = discovery.build('analyticsreporting', 'v4', credentials=credentials)
-    return analytics
+    def initialize_analyticsreporting(self):
+        """Initializes an Analytics Reporting API V4 service object.
+        Returns:
+          An authorized Analytics Reporting API V4 service object.
+        """
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(self.KEY_FILE_LOCATION, self.SCOPES)
+        # Build the service object.
+        analytics = discovery.build('analyticsreporting', 'v4', credentials=credentials)
+        return analytics
 
+    def get_report(self, analytics):
+        """Queries the Analytics Reporting API V4.
+        Args:
+          analytics: An authorized Analytics Reporting API V4 service object.
+        Returns:
+          The Analytics Reporting API V4 response.
+        """
+        return analytics.reports().batchGet(
+            body={
+                'reportRequests': [
+                    {
+                        'viewId': self.VIEW_ID,
+                        'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
+                        'metrics': [{'expression': 'ga:users'},
+                                    {"expression": "ga:pageviews"},
+                                    {"expression": "ga:sessions"}],
+                        'dimensions': [{'name': 'ga:city'},
+                                       {"name": "ga:browser"}],
+                        "orderBys":
+                            [
+                                {"fieldName": "ga:sessions", "sortOrder": "DESCENDING"},
+                                {"fieldName": "ga:pageviews", "sortOrder": "DESCENDING"},
+                            ]
+                    }]
+            }
+        ).execute()
 
-def get_report(analytics):
-    """Queries the Analytics Reporting API V4.
-    Args:
-      analytics: An authorized Analytics Reporting API V4 service object.
-    Returns:
-      The Analytics Reporting API V4 response.
-    """
-    return analytics.reports().batchGet(
-        body={
-            'reportRequests': [
-                {
-                    'viewId': VIEW_ID,
-                    'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
-                    'metrics': [{'expression': 'ga:users'},
-                                {"expression": "ga:pageviews"},
-                                {"expression": "ga:sessions"}],
-                    'dimensions': [{'name': 'ga:city'},
-                                   {"name": "ga:browser"}],
-                    "orderBys":
-                        [
-                            {"fieldName": "ga:sessions", "sortOrder": "DESCENDING"},
-                            {"fieldName": "ga:pageviews", "sortOrder": "DESCENDING"},
-                        ]
-                }]
-        }
-    ).execute()
+    def print_response(self, response):
+        """Parses and prints the Analytics Reporting API V4 response.
 
+        Args:
+          response: An Analytics Reporting API V4 response.
+        """
+        for report in response.get('reports', []):
+            columnHeader = report.get('columnHeader', {})
+            dimensionHeaders = columnHeader.get('dimensions', [])
+            metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
 
-def print_response(response):
-    """Parses and prints the Analytics Reporting API V4 response.
+            for row in report.get('data', {}).get('rows', []):
+                dimensions = row.get('dimensions', [])
+                dateRangeValues = row.get('metrics', [])
 
-    Args:
-      response: An Analytics Reporting API V4 response.
-    """
-    for report in response.get('reports', []):
-        columnHeader = report.get('columnHeader', {})
-        dimensionHeaders = columnHeader.get('dimensions', [])
-        metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+                for header, dimension in zip(dimensionHeaders, dimensions):
+                    print(header + ': ' + dimension)
 
-        for row in report.get('data', {}).get('rows', []):
-            dimensions = row.get('dimensions', [])
-            dateRangeValues = row.get('metrics', [])
+                for i, values in enumerate(dateRangeValues):
+                    print('Date range: ' + str(i))
+                    for metricHeader, value in zip(metricHeaders, values.get('values')):
+                        print(metricHeader.get('name') + ': ' + value)
 
-            for header, dimension in zip(dimensionHeaders, dimensions):
-                print(header + ': ' + dimension)
-
-            for i, values in enumerate(dateRangeValues):
-                print('Date range: ' + str(i))
-                for metricHeader, value in zip(metricHeaders, values.get('values')):
-                    print(metricHeader.get('name') + ': ' + value)
-
-
-def main():
-    analytics = initialize_analyticsreporting()
-    response = get_report(analytics)
-    print_response(response)
+    def main(self):
+        analytics = self.initialize_analyticsreporting()
+        response = self.get_report(analytics)
+        response_data = self.print_response(response)
+        print(response_data)
 
 
 if __name__ == '__main__':
-    main()
+    google_data = GoogleApi()
+    google_data.main()
 
 
