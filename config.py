e9808786ab7aa76cc667f5403fd5acdf43574a84
Wu Charles <wcadaydayup@163.com>
2019-06-03 12:10:15 +0800

    修改回调url

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ logger = logging.getLogger()
 SHOPIFY_CONFIG = {
     "client_id": "f9cd4d9b7362ae81038635518edfd98f",
     "client_secret": "a7159ff763aadb8f6c047563dbbe73e1",
-    "redirect_uri": "https://pinbooster.seamarketings.com/api/v1/shopify/callback/",
+    "redirect_uri": "https://pinbooster.seamarketings.com/api/v1/auth/shopify/callback/",
     "scopes": ["read_content", "write_content", "read_themes", "write_themes", "read_products",
                "write_products", "read_product_listings", "read_customers", "write_customers",
                "read_orders", "write_orders", "read_shipping", "write_draft_orders", "read_inventory",
@@ -28,7 +28,7 @@ PINTEREST_CONFIG = {
     "client_id": "5031224083375764064",
     "client_secret": "c3ed769d9c5802a98f7c4b949f234c482a19e5bf3a3ac491a0d20e44d7f7556e",
     "scope": "read_public,write_public,read_relationships,write_relationships",
-    "redirect_uri": "https://pinbooster.seamarketings.com/api/v1/pinterest/callback/"
+    "redirect_uri": "https://pinbooster.seamarketings.com/api/v1/auth/pinterest/callback/"
 }
 
 GA_CONFIG = {
