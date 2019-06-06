bde1eaf4ac7c0cf48215e4d1d80442d6c83b3503
Leemon7 <limengqiAliase@163.com>
2019-06-06 15:37:10 +0800

    fix bug

--- sea_app/views/dashboard.py
+++ sea_app/views/dashboard.py
@@ -19,16 +19,16 @@ class DashBoardView(APIView):
 
     def get(self, request, *args, **kwargs):
         # 过滤筛选条件
-        logger.info("dashboard trigger! ")
+        # logger.info("dashboard trigger! ")
         pin_set_list, product_set_list = reports.get_common_data(request)
         part = int(eval(kwargs["pk"]))
-        logger.info("dashboard trigger! pd={}".format(part))
+        # logger.info("dashboard trigger! pd={}".format(part))
         if part == 1:
             # 账户总览 图数据
             overview_list = reports.account_overview_chart(pin_set_list, product_set_list, request)
             # 账户总览 表数据
             total_data = reports.account_overview_table(overview_list)
-            logger.info("dashboard trigger! total_data={}".format(total_data))
+            # logger.info("dashboard trigger! total_data={}".format(total_data))
             return Response({"overview_list": overview_list, "total_data": total_data})
         elif part == 2:
             # 最新新增数据
