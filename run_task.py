86f77c18771862855c5ccef8708ca1326f8d1f52
Wu Charles <wcadaydayup@163.com>
2019-06-06 13:47:13 +0800

    run task

---
+++ run_task.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by charles on 2019-06-06
+# Function: 
+import time
+from task.task_processor import TaskProcessor
+
+
+def main():
+    tsp = TaskProcessor()
+    tsp.start_all(rule_interval=120, publish_pin_interval=240, pinterest_update_interval=3800, shopify_update_interval=3800)
+    while 1:
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
