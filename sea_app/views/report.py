71940d6b69432ba2e2ffc8a089fa90a2373da815
Leemon7 <limengqiAliase@163.com>
2019-06-13 13:59:02 +0800

    fix bug

--- sea_app/views/report.py
+++ sea_app/views/report.py
@@ -10,15 +10,11 @@ from sea_app.views import reports
 
 class DailyReportView(generics.ListAPIView):
     """日报列表展示"""
-    pagination_class = PNPagination
     permission_classes = (IsAuthenticated,)
     authentication_classes = (JSONWebTokenAuthentication,)
 
     def list(self, request, *args, **kwargs):
         data_list = reports.daily_report_view(request)
-        page = self.paginate_queryset(data_list)
-        if page is not None:
-            return self.get_paginated_response(page)
         return Response(data_list)
 
 
