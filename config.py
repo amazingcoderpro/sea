172d1b7e84e99e6bbed9a409720e669e07107db5
Wu Charles <wcadaydayup@163.com>
2019-07-24 15:23:36 +0800

    修改pin授权

--- config.py
+++ config.py
@@ -12,10 +12,8 @@ logger = logging.getLogger()
 SHOPIFY_CONFIG = {
     # "client_id": "f9cd4d9b7362ae81038635518edfd98f",#ordersea
     # "client_secret": "a7159ff763aadb8f6c047563dbbe73e1",
-    # "client_id": "7fced15ff9d1a461f10979c3eae2eca8",  # pinbooster
-    "client_id": "5039319345541466734", # Pin More Pins
-    # "client_secret": "461a69b7c8c13a37416d79769175ce23",
-    "client_secret": "ca3cad3059dd5314abf66144e0e8ce2a72b6c864092c5da3427bbbd914f2ab5d",    # Pin More Pinsr
+    "client_id": "7fced15ff9d1a461f10979c3eae2eca8",  # pinbooster
+    "client_secret": "461a69b7c8c13a37416d79769175ce23",
     "ask_permission_uri": "https://pinbooster.seamarketings.com/api/v1/auth/shopify/ask_permission/",
     "redirect_uri": "https://pinbooster.seamarketings.com/api/v1/auth/shopify/callback/",
     "scopes": ["read_content", "write_content", "read_themes", "write_themes", "read_products",
@@ -32,8 +30,8 @@ SHOPIFY_CONFIG = {
 
 
 PINTEREST_CONFIG = {
-    "client_id": "5033349102128117330",
-    "client_secret": "7578af0b0a9102815d71c822813e717b23362b0725e44e245e4296669124fe98",
+    "client_id": "5039319345541466734",
+    "client_secret": "ca3cad3059dd5314abf66144e0e8ce2a72b6c864092c5da3427bbbd914f2ab5d",
     "scope": "read_public,write_public,read_relationships,write_relationships",
     "redirect_uri": "https://pinbooster.seamarketings.com/api/v1/auth/pinterest/callback/"
 }
